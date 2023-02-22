7fab21d3aec1c9ee8a2c0a63ed14bd0b5267e140
fmwikali <fmwikali2020@gmail.com>
2023-02-22 21:24:33 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
 int main(void)
 {	
 	_putchar('_');
@@ -11,7 +16,10 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return 0;
+
+
+	return (0);
+
 }
-        
+
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,17 @@
-#ifndefMAIN_H_
-#define MAIN_H_
+#ifndefMAIN_H
+#define MAIN_H
 
-int main(int argc,char *argv[]);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int a);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int,int);
+void print_to_98(int n);
+int _putchar(char c);
 
-int_putchar(char c);
-
-3endif /*MAIN_H_ */ 
+#endif
