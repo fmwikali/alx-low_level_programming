29e80d57ebb7d0659988d7136d6f0b820903151b
fmwikali <fmwikali2020@gmail.com>
2023-03-02 22:52:09 +0300

    initial commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,16 +5,20 @@
  * @s1: string one
  * @s2: string two
  *
- * Return: negative, 0 and positive if *s1 < *s2, *s1 == *s2,
- * *s1 > *s2 respectively
+ * Return: s1[i] - s2[i],
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
+		{
 			return (s1[i] - s2[i]);
+		}
+
 	}
 	return (0);
+}
