d50f10a51ec199adaa3a3f5ed011107326d8446f
fmwikali <fmwikali2020@gmail.com>
2023-03-02 22:57:49 +0300

    initial commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
@@ -12,6 +13,6 @@ char *leet(char *n);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int _strcmp(char *s1, char *s2);
+
 
 #endif
