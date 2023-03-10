0b4817ebecc0bb21b886559c5e26569eba5ae88e
fmwikali <fmwikali2020@gmail.com>
2023-03-10 21:25:06 +0300

    initial commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_calc(int n, int x);
@@ -13,7 +14,5 @@ int is_prime_calc(int n, int x);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int palindrome(char *s, int first, int last);
-int _strlen_recursion(char *s);
 
-
-#endif 
+#endif
