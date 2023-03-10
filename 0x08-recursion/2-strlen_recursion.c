b4969c7fd9354ecb483dee72f1ad31565933e27c
fmwikali <fmwikali2020@gmail.com>
2023-03-10 22:15:32 +0300

    initial commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
+ * _strlen_recursion - returns length of string
  *
- * @s: string to be printed in reverse
+ * @s: string to be counted
+ * Return: address + 1 recusively and 0 at null terminator
  */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	
-		
-		return;
-	
+		return (0);
 
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	return (1 + _strlen_recursion(s + 1));
 }
+
